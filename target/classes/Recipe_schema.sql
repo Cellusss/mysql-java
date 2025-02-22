ed70180b4f10e8da811833f6b7116f55c928ebfd
Cellusss <marcellusjones94@yahoo.com>
2025-02-22 12:45:08 -0800

    first commit

---
+++ target/classes/Recipe_schema.sql
@@ -0,0 +1,54 @@
+DROP TABLE IF EXISTS ingredient; 
+DROP TABLE IF EXISTS step;
+DROP TABLE IF EXISTS recipe_category;
+DROP TABLE IF EXISTS unit;
+DROP TABLE IF EXISTS category;
+DROP TABLE IF EXISTS recipe; 
+
+CREATE TABLE recipe ( 
+ recipe_id INT /*auto_increment*/ NOT NULL,
+ recipe_name VARCHAR (128) NOT NULL, 
+ notes TEXT,
+ num_servings INT,
+ prep_time TIME,
+ cook_time TIME,
+ created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+PRIMARY KEY (Recipe_id)
+);
+
+
+CREATE TABLE category (
+ category_id INT /*auto_increment*/ NOT NULL,
+ category_name VARCHAR (64) NOT NULL 
+PRIMARY KEY (catergory_id)
+);
+
+CREATE TABLE unit (
+unit_id INT /*auto_increment*/ NOT NULL, 
+unit_name_singular VARCHAR (32) NOT NULL,
+unit_name_plural VARCHAR (34) NOT NULL 
+PRIMARY KEY (unit_id)
+);
+
+CREATE TABLE recipe_category (
+ recipe_id INT NOT NULL,
+ category_id INT NOT NULL 
+ 
+);
+
+CREATE TABLE step (
+ step_id INT /*auto_increment*/ NOT NULL,
+ recipe_id INT NOT NULL,
+ step_order INT NOT NULL, 
+ step_text TEXT NOT NULL
+);
+ 
+CREATE TABLE ingredient (
+ ingredient_id INT /*auto_increment*/ NOT NULL,
+ recipe_id INT NOT NULL,
+ unit_id INT NOT NULL,
+ ingredient_name VARCHAR(64) NOT NULL,
+ instruction VARCHAR(64),
+ ingredient_order INT NOT NULL,
+ amount DECIMAL (7,2)
+)
